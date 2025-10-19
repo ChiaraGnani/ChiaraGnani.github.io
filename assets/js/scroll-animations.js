dd2648525c69b1dcd865411301913e7cc35e3b74
ChiaraGnani <gnanichiara99@gmail.com>
2025-10-19 20:55:31 +0200

    v53

--- assets/js/scroll-animations.js
+++ assets/js/scroll-animations.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const sections = document.querySelectorAll(
-    ".section-1, .section-2, .cs-section-1, .cs-section-2, .dco-section-1, .dco-section-2, .dco-section-3, .esp-container"
+    ".s1-wrapper, .s2-wrapper, .s3-wrapper"
   );
 
   const observerOptions = {
